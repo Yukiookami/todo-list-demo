0d70f49af8f1a2e99620c3f42c92114be3199ef0
Yukiookami <37170227+Yukiookami@users.noreply.github.com>
2022-04-18 07:55:56 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: zxy
+ * @Date: 2022-04-10 10:40:51
+ * @LastEditTime: 2022-04-18 07:43:43
+ * @FilePath: /todo-list-demo/src/App.vue
+-->
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="scss">
+@import './assets/css/_variables.scss';
+
+* {
+  font-family: $todo-list-font-family;
+  font-size: $todo-list-font-size;
+}
+
+body {
+  background-color: $todo-list-background-color;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: zxy
+ * @Date: 2022-04-10 10:40:51
+ * @LastEditTime: 2022-04-18 07:08:27
+ * @FilePath: /todo-list-demo/src/main.js
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'element-plus/dist/index.css'
+
+createApp(App).mount('#app')
