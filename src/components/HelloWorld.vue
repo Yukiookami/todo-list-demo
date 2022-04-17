2574265cbe434ba14ac65bd333f42327f35b1d9f
Yukiookami <37170227+Yukiookami@users.noreply.github.com>
2022-04-18 07:55:23 +0900

    增加少量html代码，增加css变量

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,37 @@
 <!--
  * @Author: zxy
  * @Date: 2022-04-10 10:40:51
- * @LastEditTime: 2022-04-16 12:09:20
+ * @LastEditTime: 2022-04-18 07:53:45
  * @FilePath: /todo-list/src/components/HelloWorld.vue
 -->
 <template>
-  <h1>{{ title }}</h1>
-  
-  <section>
-    <input type="text" v-model="state.inputValue">
-    <!-- todoItem组件 -->
-    <div></div>
-    <!-- todoItem end -->
-
-    <div>
-      <div>
-        <span>{{ state.cont }} items left</span>
+  <div class="todo-list-main-sec">
+    <h1 class="todo-list-title">{{ title }}</h1>
+    
+    <section class="todo-list-content-sec">
+      <div class="todo-list-input-box">
+        <input class="todo-list-input" type="text" placeholder="What needs to be done" v-model="state.inputValue">
+        <label class="todo-list-clicl-all" ></label>
       </div>
-
-      <div>
-        <span>All</span>
-        <span>Active</span>
-        <span>Complete</span>
-      </div>
-
+      <!-- todoItem组件 -->
       <div></div>
-    </div>
-  </section>
+      <!-- todoItem end -->
+
+      <div class="todo-list-footer">
+        <div class="todo-list-text-button-box">
+          <span>{{ state.cont }} items left</span>
+        </div>
+
+        <div>
+          <span class="filter-button click">All</span>
+          <span class="filter-button">Active</span>
+          <span class="filter-button">Complete</span>
+        </div>
+
+        <div class="todo-list-text-button-box"></div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
